c9eae4bfb98684b63a60d3b45fe98c9fcec80190
nduytung <nduytung.1611@gmail.com>
2021-10-07 08:26:49 +0700

    add login & landing route

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,17 @@ import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserROuter as Router, Switch, Route } from "react-router-dom";
-function App() {
+import Landing from "./components/Landing";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
